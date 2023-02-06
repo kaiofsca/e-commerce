4824c97a5c6dd850be399ebe5fd3f9c089e7b909
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-02-06 10:45:08 -0300

    a

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BiBorderAll } from 'react-icons/bi'
+import { BsChevronDown } from 'react-icons/bs'
 
 const Navbar = () => {
   return (
     <>
-        <h1>Navbar</h1>
+        <header className="header">
+          <div className="container d_flex">
+            <div className="categories d_flex">
+              <span><BiBorderAll/></span>
+              <h4> Categories <BsChevronDown/> </h4>
+            </div>
+
+            <div className="navLink">
+              <ul className='nav'></ul>
+            </div>
+          </div>
+        </header>
     </>
   )
 }
